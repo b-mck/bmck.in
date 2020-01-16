aa91988a77d95c2981b2a34d6a5266a234f3bba1
Benjamin McKinney <denali.99755+github@gmail.com>
2020-01-15 19:36:05 -0800

    add nodemon

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
 const server = express();
+express.static('static');
 server.get('/', (req, res) => res.send('hello world !'));
 server.listen(3000);
+console.log('listening on port 3000');
