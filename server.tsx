b566ffcb20b5bdc7e77a6084c45a99baae05ba30
Benjamin McKinney <denali.99755+github@gmail.com>
2020-03-15 13:27:14 -0700

    ssr

--- server.tsx
+++ server.tsx
@@ -1,6 +1,11 @@
 import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+import Resume from './resume';
 
 const server = express();
+server.get('/resume', (req, res) => res.send(renderToString(<Resume />)));
 server.use(express.static('static', { fallthrough: true }));
 server.get('/', (req, res) => res.send('hello world !'));
 server.listen(3000);
